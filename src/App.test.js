6fb5c829a1d606f215320a6c179e911d2385c156
baltazarparra <baltazarparra@outlook.com>
2017-07-11 07:07:40 -0300

    Falhando miseravelmente ao tentar implementar uma suite de testes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,68 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types'
 import App from './App';
+import { shallow, mount, render } from 'enzyme'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+import Checkout from './components/checkout'
+import Done from './components/done'
+import Footer from './components/footer'
+import Header from './components/header'
+import Modal from './components/modal'
+import Plans from './components/plans'
+import Price from './components/price'
+
+describe('<App />', () => {
+  it('renders 1 <App /> component', () => {
+    const component = shallow(<App />)
+    expect(component).toHaveLength(1)
+  })
+})
+
+describe('<Checkout />', () => {
+  it('renders 1 <Checkout /> component', () => {
+    const component = shallow(<Modal />)
+    expect(component).toHaveLength(1)
+  })
+})
+
+describe('<Done />', () => {
+  it('renders 1 <Done /> component', () => {
+    const component = shallow(<Done />)
+    expect(component).toHaveLength(1)
+  })
+})
+
+describe('<Footer />', () => {
+  it('renders 1 <Footer /> component', () => {
+    const component = shallow(<Footer />)
+    expect(component).toHaveLength(1)
+  })
+})
+
+describe('<Header />', () => {
+  it('renders 1 <Header /> component', () => {
+    const component = shallow(<Header />)
+    expect(component).toHaveLength(1)
+  })
+})
+
+describe('<Modal />', () => {
+  it('renders 1 <Modal /> component', () => {
+    const component = shallow(<Modal />)
+    expect(component).toHaveLength(1)
+  })
+})
+
+describe('<Plans />', () => {
+  it('renders 1 <Plans /> component', () => {
+    const component = shallow(<Plans />)
+    expect(component).toHaveLength(1)
+  })
+})
+
+describe('<Price />', () => {
+  it('renders 1 <Price /> component', () => {
+    const component = shallow(<Price />)
+    expect(component).toHaveLength(1)
+  })
+})
