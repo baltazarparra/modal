f31ecb65cf7d2a8764f44d56fc7221d05d3eb8fd
baltazarparra <baltazarparra@outlook.com>
2017-07-11 07:45:29 -0300

    Começando versão desktop
    Começando a posicionar os elementos para versão acima de 768px

--- src/components/header.js
+++ src/components/header.js
@@ -5,6 +5,10 @@ const Header = ({pageActive, handleBack}) => (
     {pageActive === 'checkout' && <button className="header__button header__button--back" onClick={handleBack}> 〈 </button>}
     <h1 className="header__title">Assine o Sexlog VIP</h1>
     <button className="header__button"> ✕ </button>
+    <div className="header__breadcrumb">
+      <p className="header__description header__description--active">Escolhe o plano ideal para você</p>
+      <p className="header__description">Escolha a forma de pagamento</p>
+    </div>
   </header>
 )
 
