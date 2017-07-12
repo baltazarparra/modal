e926811e0d56dbce8443ca323f9088487dcc12a6
baltazarparra <baltazarparra@outlook.com>
2017-07-11 23:47:18 -0300

    Ajustes finais

--- src/components/header.js
+++ src/components/header.js
@@ -5,10 +5,16 @@ const Header = ({pageActive, handleBack}) => (
     {pageActive === 'checkout' && <button className="header__button header__button--back" onClick={handleBack}> 〈 </button>}
     <h1 className="header__title">Assine o Sexlog VIP</h1>
     <button className="header__button"> ✕ </button>
+    {pageActive === 'home' &&
     <div className="header__breadcrumb">
-      <p className="header__description header__description--active">Escolhe o plano ideal para você</p>
+      <p className="header__description active">Escolhe o plano ideal para você</p>
       <p className="header__description">Escolha a forma de pagamento</p>
-    </div>
+    </div>}
+    {pageActive === 'checkout' &&
+    <div className="header__breadcrumb">
+      <p className="header__description">Escolhe o plano ideal para você</p>
+      <p className="header__description active">Escolha a forma de pagamento</p>
+    </div>}
   </header>
 )
 
